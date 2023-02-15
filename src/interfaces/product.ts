927c6bcbe1153a2a8f059f6be5696da2964cc85e
omodauda <omodauda.dl@gmail.com>
2023-02-15 02:14:20 +0100

    product subcategory type

--- src/interfaces/product.ts
+++ src/interfaces/product.ts
@@ -1,3 +1,10 @@
+import { Types } from 'mongoose';
+
 export interface IProductCategory {
   name: string;
+}
+
+export interface IProductSubCategory {
+  name: string;
+  category: Types.ObjectId
 }
