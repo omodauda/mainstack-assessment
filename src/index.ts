29e8c30f943ee9e473eeb72c346c48d757320b6a
omodauda <omodauda.dl@gmail.com>
2023-02-15 16:07:22 +0100

    use product route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import dotenv from 'dotenv';
 import App from './app';
 
+import ProductRoute from './routes/product.route';
+
 dotenv.config();
 
 
-const app = new App();
+const app = new App([
+  new ProductRoute()
+]);
 
 app.listen();
 app.connectDatabase();
