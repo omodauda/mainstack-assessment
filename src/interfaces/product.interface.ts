17df2101552702eab8e1514c0c53457f38805a17
omodauda <omodauda.dl@gmail.com>
2023-02-16 12:37:10 +0100

    product type interface

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -2,10 +2,25 @@ import { Types } from 'mongoose';
 
 export interface IProductCategory {
   name: string;
-  subcategories?: [Types.ObjectId]
 }
 
 export interface IProductSubCategory {
   name: string;
   category: Types.ObjectId
+}
+
+export interface IProduct {
+  category: Types.ObjectId;
+  subcategory: Types.ObjectId;
+  name: string;
+  brand: string;
+  description: string;
+  price: number;
+  quantity: number;
+  images: [IProductImages]
+}
+
+export interface IProductImages {
+  url: string;
+  id: string;
 }
