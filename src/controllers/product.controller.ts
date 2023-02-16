ee30f6f1e6edc7a03e5b3c9454df84593dc0117c
omodauda <omodauda.dl@gmail.com>
2023-02-16 14:24:27 +0100

    create product controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,12 @@
 import { Response, Request, NextFunction } from 'express';
+import Product from 'src/database/models/Product';
 import ProductCategory from 'src/database/models/ProductCategory';
+import ProductSubCategory from 'src/database/models/ProductSubCategory';
+import Cloudinary from 'src/utils/cloudinary';
+import HttpException from 'src/utils/handlers/error.handler';
 
 export default class ProductController {
+  private Cloudinary = new Cloudinary();
 
   public getCategories = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -14,6 +19,50 @@ export default class ProductController {
           data: categories
         });
     } catch (error) {
+      next(error);
+    }
+  };
+
+  public createProduct = async (req: Request, res: Response, next: NextFunction) => {
+    const { category, subcategory, name, brand, description, price, quantity } = req.body;
+    try {
+      if (!req.files.length || !Array.isArray(req.files)) {
+        throw new HttpException(400, 'no image file selected');
+      };
+      const existingCategory = await ProductCategory.findById(category);
+      if (!existingCategory) {
+        throw new HttpException(400, 'invalid product category');
+      }
+      const existingSubCategory = await ProductSubCategory.findById(subcategory);
+
+      if (!existingSubCategory || existingCategory.id !== existingSubCategory.category._id.toString()) {
+        throw new HttpException(400, 'invalid product subcategory');
+      }
+
+      const images = req.files;
+      const imageData = [];
+      for (const image of images) {
+        const { public_id, secure_url } = await this.Cloudinary.uploadImage(image);
+        imageData.push({ public_id, url: secure_url });
+      }
+      await Product.create({
+        category,
+        subcategory,
+        name,
+        brand,
+        description,
+        price,
+        quantity,
+        images: imageData
+      });
+      return res
+        .status(201)
+        .json({
+          status: 'success',
+          message: 'product created successfully'
+        });
+    } catch (error) {
+      console.log(error);
       next(error);
     }
   };
